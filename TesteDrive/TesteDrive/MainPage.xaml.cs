fe246e88fb68434f2dd327104f0f74b8f7c7d182
Ryan Lemos <ryanlucas.lemos@gmail.com>
2020-06-14 17:09:13 -0300

    Inclui o binding da lista de veiculos

--- TesteDrive/TesteDrive/MainPage.xaml.cs
+++ TesteDrive/TesteDrive/MainPage.xaml.cs
@@ -9,19 +9,29 @@ using Xamarin.Forms;
 
 namespace TesteDrive
 {
+    public class Veiculo
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        public List<Veiculo> Veiculos { get; set; }
         public MainPage()
         {
             InitializeComponent();
-
-            listViewVeiculos.ItemsSource = new string[]
+            Veiculos = new List<Veiculo>()
             {
-                "Fiesta", "HB20", "Civic"
+                new Veiculo() {Nome = "Fiesta", Preco = 40000  },
+                new Veiculo() {Nome =  "HB20", Preco = 50000  },
+                new Veiculo() {Nome = "Civic", Preco = 60000  },
             };
+
+            BindingContext = this;
         }
     }
 }
